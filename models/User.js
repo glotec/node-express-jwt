31e00310a30ca44ca8ce4078a82ae0f054e7d828
glotec <gloireshukurubahati@gmail.com>
2023-11-21 14:47:43 +0200

    model: User model using hooks

--- models/User.js
+++ models/User.js
@@ -1,17 +1,36 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valide email']
     },
     password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters']
     }
+});
+
+/********* Hooks Mongodb************ */
+
+// fire a function after doc saved to db 
+userSchema.post('save', function (doc, next) {
+    console.log('new user was created', doc);
+    next();
+});
+
+// fire a function before doc saved to db 
+userSchema.pre('save', async function (next) {
+    // console.log('user about to be created & saved', this);
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 const User = mongoose.model('user', userSchema);
